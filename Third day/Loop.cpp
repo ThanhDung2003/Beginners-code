661575754a4aeb1ead47325b3f1e700ebb5d6b45
ThanhDung2003 <thanhdungkimtha@gmail.com>
2024-05-21 13:53:07 +0700

    Done loop

--- Third day/Loop.cpp
+++ Third day/Loop.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 void ShowMenu()
@@ -65,25 +66,76 @@ void ReverseNumber()
     cout << "\nThe reversed number: " << reversed;
 }
 
+void Fibo(int &Fibo_1, int &Fibo_2)
+{
+    int temp = Fibo_1;
+    Fibo_1 = Fibo_1 + Fibo_2;
+    Fibo_2 = temp;
+    cout << Fibo_1 << "\t";
+}
+
 void Fibonacci()
 {
-    cout << "How many numbers you want print?";
+    cout << "\nHow many Fibo numbers you want to print?\n";
     int number;
     cin >> number;
     int Fibo_1 = 0, Fibo_2 = 1;
-    cout << Fibo_1 << " " << Fibo_2;
-    for (int i = 2; i <= number; i++)
+    for (int i = 1; i <= number; i++)
     {
-        cout << Fibo_1 + Fibo_2 << " ";
+        Fibo(Fibo_1, Fibo_2);
+    }
+}
+
+void DrawTriangle()
+{
+    int width;
+    cout << "\nWhat is the width of the triangle?\n**Note: odd number**\n";
+    cin >> width;
+    while (width % 2 == 0)
+    {
+        cout << "\nPlease enter the odd number.\n";
+        cin >> width;
+    }
+    // In phần trên của tam giác
+    for (int i = 1; i <= (width / 2) + 1; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            cout << "*";
         }
+        cout << endl;
+    }
+
+    // In phần dưới của tam giác
+    for (int i = (width / 2); i > 0; --i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
-void DrawRectangle()
+bool Retry(char &continued)
 {
-}
-
-void Retry()
-{
+    cout << "\nDo you want continue (y/n)?\n";
+    cin >> continued;
+    if (continued == 'n')
+    {
+        cout << "End.\n";
+        return false;
+    }
+    else if (continued == 'y')
+    {
+        cout << "Restart.\n";
+        return true;
+    }
+    else
+    {
+        cout << "Try again: ";
+        Retry(continued);
+    }
 }
 
 int main()
@@ -109,13 +161,17 @@ int main()
             Fibonacci();
             break;
         case 5:
-            DrawRectangle();
+            DrawTriangle();
             break;
         case 6:
             return 0;
         default:
             cout << "\nTry again.\n";
         }
-        Retry();
+        if (Retry(continued) == true)
+            continue;
+        else if (Retry(continued) == false)
+            break;
     }
+    return 0;
 }
